65d8b16df49bbbffb5a21d4360138b750b632be7
LongVu224 <vuhailong224@gmail.com>
2019-10-13 12:04:13 +0300

    Add mongo atlas

--- app.js
+++ app.js
@@ -17,7 +17,12 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes     = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp",{ useNewUrlParser: true });
+mongoose.connect("mongodb+srv://jrbalrog96:gavit6436@cluster0-8xw3j.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true, useCreateIndex: true })
+        .then(() => {
+            console.log("Connected to Mongodb");
+        }).catch(err => {
+            console.log('ERROR:', err.message);
+        })
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
