3c138ca8d3704facd1a0b7893e147ae9bb1a4c79
LongVu224 <vuhailong224@gmail.com>
2020-09-11 20:12:15 +0300

    upgrade login UI

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, '0.0.0.0', function(){
     console.log("The YelpCamp server has started!");
 });
